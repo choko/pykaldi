f0bdb08eab2edd93400130615c020a53188feaed
Ondrej Platek <ondrej.platek@seznam.cz>
2014-02-05 14:16:45 +0100

    moved flags to python for static compilation
    probably too complex

--- src/pykaldi/setup.py
+++ src/pykaldi/setup.py
@@ -10,7 +10,7 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from subprocess import check_output
 
-STATIC = False
+STATIC = True
 
 install_requires = []
 if python_version < (2, 7):
@@ -24,20 +24,52 @@ ext_modules = []
 if STATIC:
     # STATIC TODO extract linking parameters from Makefile
     library_dirs, libraries = [], []
-    extra_objects = ['pykaldi.a', ]
+    extra_objects = ['../dec-wrap/dec-wrap.a', '../decoder/kaldi-decoder.a',
+                     '../thread/kaldi-thread.a', '../lat/kaldi-lat.a',
+                     '../hmm/kaldi-hmm.a', '../transform/kaldi-transform.a',
+                     '../gmm/kaldi-gmm.a', '../fstext/kaldi-fstext.a',
+                     '../tree/kaldi-tree.a', '../matrix/kaldi-matrix.a',
+                     '../feat/kaldi-feat.a', '../util/kaldi-util.a',
+                     '../base/kaldi-base.a', ]
+# ATLASLIBS = /usr/lib/atlas-base/libatlas.so.3gf
+# /usr/lib/atlas-base/libf77blas.so.3gf
+# /usr/lib/atlas-base/libcblas.so.3gf
+# /usr/lib/atlas-base/liblapack_atlas.so.3gf
+    ATLASLIBS = ['/usr/lib/atlas-base/libatlas.so.3gf', '/usr/lib/atlas-base/libf77blas.so.3gf',
+                 '/usr/lib/atlas-base/libcblas.so.3gf', '/usr/lib/atlas-base/liblapack_atlas.so.3gf']
+
+    # ATLASLIBS = ['atlas', 'f77blas',
+    #              'cblas', 'lapack_atlas']
+    ATLASINC = '/ha/work/people/oplatek/kaldi/tools/ATLAS/include'
+    libraries = ['fst']
+    libraries.extend(ATLASLIBS)
+    print libraries
+    ext_modules.append(Extension('pykaldi.decoders',
+                                 language='c++',
+                                 include_dirs=['..', 'pyfst', ATLASINC],
+                                 library_dirs=['/ha/work/people/oplatek/kaldi/tools/openfst/lib',
+                                               '/usr/lib/atlas-base'],
+                                 libraries=libraries,
+                                 extra_objects=extra_objects,
+                                 extra_compile_args=['-fPIC', '-DKALDI_DOUBLEPRECISION=0',
+                                                     '-DHAVE_POSIX_MEMALIGN', '-Wno-sign-compare', '-Wno-unused-local-typedefs',
+                                                     '-Winit-self', '-DHAVE_EXECINFO_H=1', '-DHAVE_CXXABI_H',
+                                                     '-DHAVE_ATLAS'],
+                                 sources=['pykaldi/decoders.pyx', ],
+                                 ))
 else:
     # DYNAMIC
     library_dirs = ['.', ]
     libraries = ['pykaldi', ]
     extra_objects = []
-ext_modules.append(Extension('pykaldi.decoders',
-                             language='c++',
-                             include_dirs=['..', 'pyfst', ],
-                             library_dirs=library_dirs,
-                             libraries=libraries,
-                             extra_objects=extra_objects,
-                             sources=['pykaldi/decoders.pyx', ],
-                             ))
+    ext_modules.append(Extension('pykaldi.decoders',
+                                 language='c++',
+                                 include_dirs=['..', 'pyfst', ],
+                                 library_dirs=library_dirs,
+                                 libraries=libraries,
+                                 extra_objects=extra_objects,
+                                 sources=['pykaldi/decoders.pyx', ],
+                                 ))
 
 
 long_description = open(path.join(path.dirname(__file__), 'README.rst')).read()
